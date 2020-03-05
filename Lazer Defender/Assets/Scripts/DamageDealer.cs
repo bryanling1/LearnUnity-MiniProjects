a9ccfc31d932462e1d0f3ffba8ec18e50acc7aab
b3ling <b3ling@uwaterloo.ca>
2020-03-05 16:47:28 -0500

    added enemy health and subtract health on lazer trigger

---
+++ Lazer Defender/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+    // Start is called before the first frame update
+    public int getDamage(){
+        return damage;
+    }
+
+    public void hit(){
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Destroy(gameObject);
+    }
+}
