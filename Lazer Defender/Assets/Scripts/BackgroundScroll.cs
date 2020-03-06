09de3ff116c062d4f9c7e84c564b5d48fbd416e8
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-06 16:52:31 -0500

    Added moving background with Quad

---
+++ Lazer Defender/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField] float backgroundSpeed = 1f;
+    Material myMaterial;
+    Vector2 offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material; 
+        offSet = new Vector2(0f, backgroundSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
