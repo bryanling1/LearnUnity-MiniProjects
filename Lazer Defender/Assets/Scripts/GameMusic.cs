1be0b3e7e155467df67d13caeba62b9f8d8ba934
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-13 11:07:24 -0400

    Added game music singleton

---
+++ Lazer Defender/Assets/Scripts/GameMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    // Start is called before the first frame updat
+    void Awake(){
+        int sessions = FindObjectsOfType(GetType()).Length;
+        if(sessions > 1){
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }else{
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+
+}
