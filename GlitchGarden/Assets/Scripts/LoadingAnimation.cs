d908dac75e7d68ac886f82b54f4ac863c5067af4
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-14 10:41:07 -0400

    Added project Glitch Garden

---
+++ GlitchGarden/Assets/Scripts/LoadingAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] string loadingText = "loading";
+    [SerializeField] int dots = 3;
+    
+    string currentText;
+    void Start()
+    {
+        currentText = loadingText;
+        StartCoroutine(loadDotsAnimation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator loadDotsAnimation(){
+        int i = 0;
+        while(true){
+            i++;
+            displayText(currentText);
+            if(i>dots){
+                currentText = loadingText;
+                i = 0; 
+            }else{
+                currentText += ".";
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void displayText(string s){
+        GetComponent<Text>().text = currentText; 
+    }
+
+
+}
---
+++ GlitchGarden/Assets/Scripts/SceneSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : MonoBehaviour
+{
+
+    [SerializeField] float timeToLoad = 4f;
+    int currentScene = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(currentScene == 0){
+            StartCoroutine(finishLoadingSplash());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadNextLevel(){
+        int currentScene = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    IEnumerator finishLoadingSplash(){
+        yield return new WaitForSeconds(timeToLoad);
+        loadNextLevel();
+    }
+
+
+}
