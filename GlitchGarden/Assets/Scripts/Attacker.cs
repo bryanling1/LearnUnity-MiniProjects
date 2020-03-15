b72b186216fb0453c7a9f8a8f5cdd424f88325bc
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-15 15:11:10 -0400

    Added lizard moveLeft

---
+++ GlitchGarden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    
+    [SerializeField] 
+    [Range(0f, 5f)] float moveSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveLeft();
+    }
+
+    private void moveLeft(){
+        transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+    }
+}
