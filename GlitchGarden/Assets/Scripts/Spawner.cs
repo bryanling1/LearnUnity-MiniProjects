bf06f6dd4ac32b2ad6c6301b5bfb9901c5e2f2b1
b3ling <b3ling@edu.uwaterloo.ca>
2020-03-16 13:23:36 -0400

    Added enemy spanwer

---
+++ GlitchGarden/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float minRandomSpawnTime = 1f;
+    [SerializeField] float maxRandomSpawnTime = 3f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+    void Start()
+    {
+        StartCoroutine(spawnAttacker());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnAttacker(){
+        while(spawn){
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minRandomSpawnTime, maxRandomSpawnTime));
+            Instantiate(attackerPrefab, transform.position, transform.rotation);
+        }
+    }
+}
